334e2292ce477c443d2f5946700ebb58775766b3
Bijay Regmi <wilson.blackman@yahoo.com>
2019-05-31 14:29:18 +0000

    adjusted output on rejected api call on api.php

--- api.php
+++ api.php
@@ -2,8 +2,7 @@
 $api = $_GET['apikey'];
 if ($api != 'shankarboro'){
 die('<font color="red">Access Denied! No or wrong API Key.</font>
-<p> Correct syntax of this api: api.php?apikey=< your secret apikey>&code=< code > </p>
-<div><iframe width="560" height="315" src="https://www.youtube.com/embed/7w4DK-36fck?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>');
+<p> Correct syntax of this api: api.php?apikey=< your secret apikey>&code=< code > </p>');
 }
 $randomstring = base64_encode(random_bytes(10));
 $usercode = $_GET['code'];
